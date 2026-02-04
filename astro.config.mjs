6559b879adce24aa56e69b515d48e1c6a9130b00
Jonathan Matthey <mattheyj@gmail.com>
2026-02-04 13:25:00 +0100

    output static - not server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://interledger.org',
-  output: 'server',
+  output: 'static',
   prerender: {
     default: true
   },
