4f2b0efa89ac3bf1f504fb771542629042a88386
Anca Matei <ancamatei2012@gmail.com>
2026-02-05 10:33:55 +0200

    tweaks

--- scripts/import-drupal-articles.ts
+++ scripts/import-drupal-articles.ts
@@ -20,7 +20,7 @@ async function fetchTags() {
     const result = await response.json()
     return result.data.map((tag) => ({ id: tag.id, name: tag.attributes.name }))
   } catch (err) {
-    console.error('Error: ', err.message)
+    console.error(`Error inside fetchTags: ${err.message}`)
     return []
   }
 }
@@ -61,7 +61,8 @@ async function htmlToMarkdown(html: string) {
   const replacements = await Promise.all(
     matches.map(async ([fullMatch, mediaId]) => {
       const mediaUrl = mediaUrlBase + mediaId
-      const media = await fetchMedia(mediaUrl)
+      // <drupal-media> only displays data-entity-type="media", I will assume they are images
+      const media = await fetchMedia(mediaUrl, 'media--image')
       return {
         fullMatch,
         markdown: `![${media?.alt ?? ''}](${media?.url ?? ''})\n\n`
@@ -132,52 +133,71 @@ async function saveImageToDisk(url) {
   return imgReferencePath + fileName
 }
 
-async function fetchMedia(imageAPILink) {
+async function fetchMedia(mediaAPILink, type) {
   try {
-    const mediaResponse = await fetch(imageAPILink)
+    const mediaResponse = await fetch(mediaAPILink)
     if (!mediaResponse.ok) {
       throw new Error(`Response status: ${response.status}`)
     }
     const mediaResult = await mediaResponse.json()
-    const imageName = mediaResult.data.attributes.name
-    const imageAlt =
-      mediaResult.data.relationships.field_media_image.data.meta.alt ?? ''
+    const mediaName = mediaResult.data.attributes.name
+    let mediaAlt; 
+    
+    switch (type) {
+      case 'media--image': 
+        mediaAlt = mediaResult.data.relationships.field_media_image.data.meta.alt ?? '';
+        break; 
+      case 'media--svg':
+        mediaAlt = mediaResult.data.relationships.field_media_svg.data.meta.alt ?? '';
+        break;
+      default: 
+        mediaAlt = undefined;
+    }
 
-    if (imageName.includes(' ')) {
-      console.error(`${imageName} contains spaces!!!`)
+    if (mediaName.includes(' ')) {
+      console.error(`${mediaName} contains spaces!!! Please download manually`)
       return
     }
 
-    const imageUrl = drupalMediaUrl + imageName
-
+    const imageUrl = drupalMediaUrl + mediaName
+    if (type === 'media--remote_video') {
+      // don't save on disk, return embedded video link
+      return {
+        url: mediaResult.data.attributes.field_media_oembed_video,
+        alt: undefined
+      } 
+    }
     return {
       url: await saveImageToDisk(imageUrl),
-      alt: imageAlt
+      alt: mediaAlt
     }
   } catch (err) {
-    console.error('Error: ', err.message)
+    console.error(`Error in fetchMedia regarding image: ${mediaAPILink} ; ${err.message} `)
   }
 }
 
 async function getMediaInfo(media) {
   const { type, id } = media
 
-  let imageAPILink
+  let mediaAPILink
   switch (type) {
     case 'media--image':
-      imageAPILink = `https://staging.interledger.org/jsonapi/media/image/${id}`
+      mediaAPILink = `https://staging.interledger.org/jsonapi/media/image/${id}`
       break
     case 'media--svg':
-      imageAPILink = `https://staging.interledger.org/jsonapi/media/svg/${id}`
+      mediaAPILink = `https://staging.interledger.org/jsonapi/media/svg/${id}`
+      break
+    case 'media--remote_video':
+      mediaAPILink = `https://staging.interledger.org/jsonapi/media/remote_video/${id}`
       break
     default:
-      imageAPILink = null
+      mediaAPILink = null
       console.log('Media is not of type image or svg!')
   }
 
-  if (!imageAPILink) return
+  if (!mediaAPILink) return
 
-  return await fetchMedia(imageAPILink)
+  return await fetchMedia(mediaAPILink, type )
 }
 
 async function generateMDXContent(post) {
@@ -200,9 +220,10 @@ async function generateMDXContent(post) {
     `date: ${formatDate(post.attributes.created)}`,
     `slug: ${post.attributes.path.alias.replace('/news/', '')}`,
     featureImage?.url ? `featureImage: "${featureImage.url}"` : undefined,
-    `featureImageAlt: "${featureImage?.alt ?? ''}"`,
+    `featureImageAlt: "${featureImage?.alt ?? undefined}"`,
+    `pillar: "${post.attributes.field_pillar ?? ''}"`,
     thumbnail?.url ? `thumbnailImage: "${thumbnail.url}"` : undefined,
-    `thumbnailAlt: "${thumbnail?.alt ?? ''}"`,
+    `thumbnailAlt: "${thumbnail?.alt ?? undefined}"`,
     post.attributes.field_byline
       ? `authors:\n  - ${post.attributes.field_byline}`
       : undefined,
@@ -227,15 +248,11 @@ async function writeMDXFile(post) {
   const mdxContent = await generateMDXContent(post)
 
   fs.writeFileSync(filepath, mdxContent, 'utf-8')
-  console.log(`✅ Generated Blog Post MDX file: ${filepath}`)
+  // console.log(`✅ Generated Blog Post MDX file: ${filepath}`)
 }
 
 async function importArticlesFromDrupal() {
-  // all articles:
-  // let articleUrl = "https://staging.interledger.org/jsonapi/node/article";
-  // filtered article with multiple photos inside + author
-  let articleUrl =
-    'https://staging.interledger.org/jsonapi/node/article?filter[title]=Shanghai,%20Flip%20Phones,%20and%20Robots:%20An%20Interledger%20Engineering%20Adventure'
+  let articleUrl = "https://staging.interledger.org/jsonapi/node/article";
 
   const articles = []
   while (articleUrl) {
@@ -249,7 +266,7 @@ async function importArticlesFromDrupal() {
       articles.push(...result.data)
       articleUrl = result.links?.next?.href ?? null
     } catch (error) {
-      console.error(error.message)
+      console.error(`Error in catch block inside importArticlesFromDrupal: ${error.message}`)
     }
   }
   await Promise.all(articles.map(writeMDXFile))
