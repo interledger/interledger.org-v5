95093bcce5aaeea5c4773cc1ebef9fd0716f6d2f
Jonathan Matthey <mattheyj@gmail.com>
2026-02-09 11:54:29 +0100

    rm link validation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import netlify from '@astrojs/netlify'
 import mdx from '@astrojs/mdx'
 import tailwindcss from '@tailwindcss/vite'
 
-const enableLinkValidation =
-  process.env.CI === 'true' || process.env.STARLIGHT_LINKS_VALIDATOR === 'true'
 
 // https://astro.build/config
 export default defineConfig({
