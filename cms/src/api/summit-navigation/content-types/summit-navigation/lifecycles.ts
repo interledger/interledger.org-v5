3e65d3faa6cb5b5fb4781c1d6f86abf2750da43a
JxM <mattheyj@gmail.com>
2026-02-13 11:24:44 +0100

    update import paths

--- cms/src/api/summit-navigation/content-types/summit-navigation/lifecycles.ts
+++ cms/src/api/summit-navigation/content-types/summit-navigation/lifecycles.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { createNavigationLifecycle } from '../../../../utils/navigationLifecycle'
-import { PATHS } from '../../../../../src/utils/paths'
+import { PATHS } from '../../../../../../src/utils/paths'
 
 export default createNavigationLifecycle({
   contentTypeUid: 'api::summit-navigation.summit-navigation',
