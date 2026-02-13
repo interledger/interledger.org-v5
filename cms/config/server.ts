11bbb5e5dd1326f285f081f2a998e899de5a2e3e
Jonathan Matthey <mattheyj@gmail.com>
2026-02-13 13:43:41 +0100

    revert

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,12 +1,11 @@
 import path from 'path'
-import { getProjectRoot, PATHS } from '../../src/utils/paths'
 
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   // Point Strapi's public directory at the repo root's /public so uploads land in /public/uploads
   dirs: {
-    public: env('STRAPI_PUBLIC_DIR', path.join(getProjectRoot(), PATHS.PUBLIC))
+    public: env('STRAPI_PUBLIC_DIR', path.resolve(process.cwd(), '../public'))
   },
   app: {
     keys: env.array('APP_KEYS')
