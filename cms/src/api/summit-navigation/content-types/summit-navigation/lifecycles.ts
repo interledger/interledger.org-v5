3468d5f30daae59b6a6e8d2a69b7145d67f76605
JxM <mattheyj@gmail.com>
2026-02-13 11:12:34 +0100

    navigation -> foundation navigation

---
+++ cms/src/api/summit-navigation/content-types/summit-navigation/lifecycles.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+import { createNavigationLifecycle } from '../../../../utils/navigationLifecycle'
+import { PATHS } from '../../../../../src/utils/paths'
+
+export default createNavigationLifecycle({
+  contentTypeUid: 'api::summit-navigation.summit-navigation',
+  outputPath: path.join(PATHS.CONFIG_ROOT, PATHS.CONFIG.summitNavigation),
+})
