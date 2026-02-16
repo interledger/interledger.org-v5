e7a0c9f27c7bdd62ab203ed3e9243ccf3600681a
Jonathan Matthey <mattheyj@gmail.com>
2026-02-16 09:40:00 +0100

    mayb resolve findup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
-import starlightLinksValidator from 'starlight-links-validator'
+// import starlightLinksValidator from 'starlight-links-validator'
 import starlightFullViewMode from 'starlight-fullview-mode'
 import netlify from '@astrojs/netlify'
 import mdx from '@astrojs/mdx'
