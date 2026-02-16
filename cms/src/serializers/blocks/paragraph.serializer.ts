c738009d0f2d5aa82d6c96ff0c3cc3141c212dc9
JxM <mattheyj@gmail.com>
2026-02-16 13:37:57 +0100

    is html check

--- cms/src/serializers/blocks/paragraph.serializer.ts
+++ cms/src/serializers/blocks/paragraph.serializer.ts
@@ -1,10 +1,16 @@
+import isHtml from 'is-html'
 import { htmlToMarkdown } from '../../utils/mdx'
 
 export function serialize(block: {
   content: string
   alignment?: string
 }): string {
-  const content = htmlToMarkdown(block.content)
+  // If content is already markdown (not HTML), use it directly
+  // Otherwise convert HTML to markdown
+  const content = isHtml(block.content) 
+    ? htmlToMarkdown(block.content)
+    : block.content
+  
   if (block.alignment && block.alignment !== 'left') {
     return `<div class="text-${block.alignment}">\n\n${content}\n\n</div>`
   }
