df0e2ce5f16e3686a16ace0b162c6fbea74429a8
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2026-02-18 10:45:28 +0200

    format

--- src/utils/stripPagination.ts
+++ src/utils/stripPagination.ts
@@ -1,6 +1,4 @@
 export default function stripPagination(path: string) {
-  const parts = path.replace(/\/$/, '').split('/');
-  return isNaN(Number(parts.at(-1)))
-    ? path
-    : parts.slice(0, -1).join('/');
+  const parts = path.replace(/\/$/, '').split('/')
+  return isNaN(Number(parts.at(-1))) ? path : parts.slice(0, -1).join('/')
 }
