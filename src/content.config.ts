0a6984019ffbc850af1c251447d9c57762a881b8
Jonathan Matthey <mattheyj@gmail.com>
2026-02-24 14:08:36 +0100

    feat: add new collection types jon/intorg-284 (#36)
    * refactor: standardize content types and layout hierarchy
    
    Content type naming:
    - Rename blog-post API to foundation-blog-post
    - Move src/content/blog/ to src/content/foundation-blog-posts/
    - Move src/content/summit/ to src/content/summit-pages/
    - Move src/content/developers/blog/ to src/content/developers-blog-posts/
    
    Layout hierarchy:
    - BaseLayout: HTML shell with slots for header/footer
    - FoundationPageLayout: extends BaseLayout with Foundation nav
    - SummitPageLayout: extends BaseLayout with Summit nav and dark theme
    - DevelopersBlogLayout/FoundationBlogLayout: extend FoundationPageLayout
    
    Removes duplicate layouts (SummitLayout)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,42 +1,14 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
 import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders'
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema'
 import { glob } from 'astro/loaders'
 import { PATHS } from './utils/paths'
 import {
+  developersBlogFrontmatterSchema,
   foundationBlogFrontmatterSchema,
   foundationPageFrontmatterSchema,
   summitPageFrontmatterSchema
 } from './schemas/content'
-
-const engBlogCollection = defineCollection({
-  loader: glob({
-    pattern: '**/[^_]*.{md,mdx}',
-    base: `./${PATHS.CONTENT_ROOT}/${PATHS.CONTENT.developersBlog}`
-  }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    slug: z.string(),
-    lang: z.string(),
-    date: z.date(),
-    ogImageUrl: z.string().optional(),
-    tags: z.array(
-      z.enum([
-        'Interledger Protocol',
-        'Open Payments',
-        'Rafiki',
-        'Releases',
-        'Updates',
-        'Web Monetization',
-        'Card Payments'
-        // Please add a matching translation in i18n/ui.ts for any new tag
-      ])
-    ),
-    authors: z.array(z.string()),
-    author_urls: z.array(z.string())
-  })
-})
 
 const foundationBlogCollection = defineCollection({
   loader: glob({
@@ -57,15 +29,23 @@ const foundationPagesCollection = defineCollection({
 const summitPagesCollection = defineCollection({
   loader: glob({
     pattern: '**/[^_]*.{md,mdx}',
-    base: `./${PATHS.CONTENT_ROOT}/${PATHS.CONTENT.summit}`
+    base: `./${PATHS.CONTENT_ROOT}/${PATHS.CONTENT.summitPages}`
   }),
   schema: summitPageFrontmatterSchema
+})
+
+const developersBlogCollection = defineCollection({
+  loader: glob({
+    pattern: '**/[^_]*.{md,mdx}',
+    base: `./${PATHS.CONTENT_ROOT}/${PATHS.CONTENT.developersBlog}`
+  }),
+  schema: developersBlogFrontmatterSchema
 })
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }), // TODO: check base now since docs loader may have wrong path
   i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
-  'engineering-blog': engBlogCollection,
+  'developers-blog': developersBlogCollection,
   'foundation-blog': foundationBlogCollection,
   'foundation-pages': foundationPagesCollection,
   'summit-pages': summitPagesCollection
