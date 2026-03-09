7ebd184502ea9c90d2851ab75996b1988c5fd67c
Jonathan Matthey <mattheyj@gmail.com>
2026-03-09 16:33:28 +0100

    feat: render paragraph blocks as <Paragraph> JSX component

--- cms/src/serializers/blocks/paragraph.serializer.ts
+++ cms/src/serializers/blocks/paragraph.serializer.ts
@@ -1,18 +1,10 @@
-import isHtml from 'is-html'
-import { htmlToMarkdown } from '../../utils/mdx'
-
 export function serialize(block: {
   content: string
   alignment?: string
 }): string {
-  // If content is already markdown (not HTML), use it directly
-  // Otherwise convert HTML to markdown
-  const content = isHtml(block.content)
-    ? htmlToMarkdown(block.content)
-    : block.content
-
-  if (block.alignment && block.alignment !== 'left') {
-    return `<div class="text-${block.alignment}">\n\n${content}\n\n</div>`
-  }
-  return content
+  const alignmentAttr =
+    block.alignment && block.alignment !== 'left'
+      ? ` alignment="${block.alignment}"`
+      : ''
+  return `<Paragraph${alignmentAttr}>\n\n${block.content}\n\n</Paragraph>`
 }
