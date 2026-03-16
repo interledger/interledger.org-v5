e26ac38660fe61f43d3b109d6296cdc40c9f70c7
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2026-03-16 11:52:47 +0200

    chore: create Summit Speakers list page

--- src/utils/summit-talks-speakers.ts
+++ src/utils/summit-talks-speakers.ts
@@ -4,30 +4,50 @@ export interface Talk {
   id: string
   title: string
 }
+export interface Speaker {
+  id: string
+  name: string
+}
 
-export function getAllTalks(year: string): Talk[] {
-  const sessions2022 = [
+export const YEARS = ['2022', '2023', '2024', '2025']
+
+export function getSpeakers(year: string): Speaker[] {
+  const baseSpeakers: Speaker[] = [
+    {
+      id: '370w173',
+      name: 'Ayden Férdeline'
+    },
+    {
+      id: '38w5579',
+      name: 'Ioana Chiorean'
+    }
+  ]
+  //get 22 speakers to see pagination
+  const speakers2022 = Array.from({ length: 11 }).flatMap(() => baseSpeakers)
+  switch (year) {
+    case '2022':
+      return speakers2022
+    default:
+      console.error(
+        'Year is not correct or speakers data is not available for that year'
+      )
+      return []
+  }
+}
+
+export function getTalks(year: string): Talk[] {
+  const baseSessions: Talk[] = [
     {
       id: '370173',
-      title: `2022 State of Interledger`
+      title: `${year} State of Interledger`
     },
     {
       id: '385579',
-      title:
-        ' 2022 TigerBeetle, a Financial Accounting Database for Interledger'
+      title: `${year} TigerBeetle, a Financial Accounting Database for Interledger`
     }
   ]
-  const sessions2023 = [
-    {
-      id: '370173',
-      title: `2023 State of Interledger`
-    },
-    {
-      id: '385579',
-      title:
-        ' 2023 TigerBeetle, a Financial Accounting Database for Interledger'
-    }
-  ]
+  const sessions2022 = Array.from({ length: 10 }).flatMap(() => baseSessions)
+  const sessions2023 = Array.from({ length: 3 }).flatMap(() => baseSessions)
   switch (year) {
     case '2022':
       return sessions2022
@@ -35,20 +55,28 @@ export function getAllTalks(year: string): Talk[] {
       return sessions2023
     default:
       console.error(
-        'Year is not correct or data is not available for that year'
+        'Year is not correct or sessions data is not available for that year'
       )
       return []
   }
 }
 
 export async function paginateSummitTalks(paginate: PaginateFunction) {
-  const years = ['2022', '2023', '2024', '2025']
-
-  return years.flatMap((year) => {
-    const talksForYear = getAllTalks(year)
+  return YEARS.flatMap((year) => {
+    const talksForYear = getTalks(year)
     return paginate(talksForYear, {
       params: { year },
       pageSize: 10
+    })
+  })
+}
+
+export async function paginateSummitSpeakers(paginate: PaginateFunction) {
+  return YEARS.flatMap((year) => {
+    const speakersForYear = getSpeakers(year)
+    return paginate(speakersForYear, {
+      params: { year },
+      pageSize: 20
     })
   })
 }
