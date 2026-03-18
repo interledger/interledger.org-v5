ca8d403473ab34b4ffab1b0f8f04b8359046f184
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2026-03-18 16:38:40 +0200

    move dummy functions into utils/extractSessionize.ts and types into types/summit.ts

---
+++ src/types/summit.ts
@@ -0,0 +1,31 @@
+export interface Talk {
+  id: string
+  title: string
+  description: string
+  startsAt: string
+  endsAt: string
+  speakers: {
+    id: string
+    name: string
+  }[]
+  translations: string[]
+  es: {
+    title: string
+    description: string
+  }
+}
+
+export interface Speaker {
+  id: string
+  name: string
+  bio: string
+  tagLine: string
+  profilePicture: string
+  es: {
+    bio: string
+  }
+  sessions: {
+    id: number
+    title: string
+  }[]
+}
