47e5561a827ca6985f456432d096c32ada4bd4e1
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2026-03-19 11:54:57 +0200

    chore: Create list pages for Summit talks and speakers (#136)
    * chore: add base logic for summit talks list page
    
    * chore: add base HTML structure to summit talks page
    
    * chore: create Summit Speakers list page
    
    * add Spanish summit list pages
    
    * add comments
    
    * refactor list pages
    
    * style(summit): remove redundant .ts extensions from imports
    
    * apply changes as per review request
    
    ---------
    
    Co-authored-by: Jonathan Matthey <mattheyj@gmail.com>

---
+++ src/utils/summit-talks-speakers.ts
@@ -0,0 +1,120 @@
+import type { PaginateFunction } from 'astro'
+
+export type Language = 'en' | 'es'
+export interface Talk {
+  id: string
+  title: string
+}
+export interface Speaker {
+  id: string
+  name: string
+}
+
+export const YEARS = ['2022', '2023', '2024', '2025']
+
+//Dummy function - ignore this for now, will be implemented in a follow up PR
+export function getSpeakers(year: string, lang: Language): Speaker[] {
+  const baseSpeakers: Speaker[] = [
+    {
+      id: '370w173',
+      name: 'Ayden Férdeline'
+    },
+    {
+      id: '38w5579',
+      name: 'Ioana Chiorean'
+    }
+  ]
+  const baseSpanishSpeakers: Speaker[] = [
+    {
+      id: '12342',
+      name: 'Lupita'
+    },
+    {
+      id: '020231',
+      name: 'Jose Armando'
+    }
+  ]
+  //get 22 speakers to see pagination
+  const speakers2022 = Array.from({ length: 11 }).flatMap(() => baseSpeakers)
+  const spanishSpeakers2022 = Array.from({ length: 5 }).flatMap(
+    () => baseSpanishSpeakers
+  )
+  switch (true) {
+    case year === '2022' && lang === 'en':
+      return speakers2022
+    case year === '2022' && lang === 'es':
+      return spanishSpeakers2022
+    default:
+      console.error(
+        'Year is not correct or speakers data is not available for that year'
+      )
+      return []
+  }
+}
+//Dummy function - ignore this for now, will be implemented in a follow up PR
+export function getTalks(year: string, lang: Language): Talk[] {
+  const baseSessions: Talk[] = [
+    {
+      id: '370173',
+      title: `${year} State of Interledger`
+    },
+    {
+      id: '385579',
+      title: `${year} TigerBeetle, a Financial Accounting Database for Interledger`
+    }
+  ]
+  const baseSpanishSessions: Talk[] = [
+    {
+      id: '3da173',
+      title: `${year} En español: State of Interledger`
+    },
+    {
+      id: '38ad79',
+      title: `${year} En español: TigerBeetle, a Financial Accounting Database for Interledger`
+    }
+  ]
+  const sessions2022 = Array.from({ length: 10 }).flatMap(() => baseSessions)
+  const spanishSessions2022 = Array.from({ length: 6 }).flatMap(
+    () => baseSpanishSessions
+  )
+  const sessions2023 = Array.from({ length: 3 }).flatMap(() => baseSessions)
+  switch (true) {
+    case year === '2022' && lang === 'en':
+      return sessions2022
+    case year === '2022' && lang === 'es':
+      return spanishSessions2022
+    case year === '2023' && lang === 'en':
+      return sessions2023
+    default:
+      console.error(
+        'Year is not correct or sessions data is not available for that year'
+      )
+      return []
+  }
+}
+
+export async function paginateSummitTalks(
+  paginate: PaginateFunction,
+  lang: Language
+) {
+  return YEARS.flatMap((year) => {
+    const talksForYear = getTalks(year, lang)
+    return paginate(talksForYear, {
+      params: { year },
+      pageSize: 10
+    })
+  })
+}
+
+export async function paginateSummitSpeakers(
+  paginate: PaginateFunction,
+  lang: Language
+) {
+  return YEARS.flatMap((year) => {
+    const speakersForYear = getSpeakers(year, lang)
+    return paginate(speakersForYear, {
+      params: { year },
+      pageSize: 20
+    })
+  })
+}
