8d5829a261c485649c19c160ff1d8ed60a64ebe9
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2026-03-19 13:37:25 +0200

    chore: link names / titles with urls and update imports

--- src/utils/summit-talks-speakers.ts
+++ src/utils/summit-talks-speakers.ts
@@ -22,3 +22,23 @@ export async function paginateSummitSpeakers(paginate: PaginateFunction) {
     })
   })
 }
+
+export async function getSpeakerPages() {
+  return YEARS.flatMap((year) => {
+    const speakersForYear = getSpeakers(year)
+    return speakersForYear.map((entry) => ({
+      params: { year, id: generateSlug(entry.name) },
+      props: { entry }
+    }))
+  })
+}
+
+export async function getSessionPages() {
+  return YEARS.flatMap((year) => {
+    const talksForYear = getTalks(year)
+    return talksForYear.map((entry) => ({
+      params: { year, id: generateSlug(entry.title) },
+      props: { entry }
+    }))
+  })
+}
