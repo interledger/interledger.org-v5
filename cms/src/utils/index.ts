471e6c1791517a00e4f2f6d9ac4ec790524fb162
JoblersTune <sarah@interledger.org>
2026-04-12 21:18:11 +0200

    chore: cleaned up cms utils index.ts

--- cms/src/utils/index.ts
+++ cms/src/utils/index.ts
@@ -5,50 +5,18 @@ export { type StrapiDocumentAPI, type StrapiAdminUser, type StrapiGlobal } from 
 export { getProjectRoot, getCmsDir, assertRunFromCms, PATHS, getContentPath, getConfigPath } from './paths'
 export { FOUNDATION_PAGE_CONTENT_POPULATE, BLOG_CONTENT_POPULATE } from './contentPopulate'
 
-// MDX & content
-export {
-  defaultLang,
-  LOCALES,
-  uidToLogLabel,
-  MATTER_STRINGIFY_OPTIONS,
-  yamlSingleQuoteScalar,
-  getImageUrl,
-  htmlToMarkdown,
-  markdownToHtml,
-  formatBlockquote,
-  getPreservedFields,
-  type HeroCta,
-  heroFrontmatter,
-  seoFrontmatter
-} from './mdx'
+// MDX generation
+export { type HeroCta, heroFrontmatter, seoFrontmatter, getPreservedFields } from './mdx'
+export { defaultLang, LOCALES, MATTER_STRINGIFY_OPTIONS, yamlSingleQuoteScalar } from './mdx'
+export { getImageUrl, htmlToMarkdown, markdownToHtml, formatBlockquote, uidToLogLabel } from './mdx'
 export { validateNoNestedJsx } from './contentValidation'
-export { removeLocalizesFromLocaleFiles, deleteLocaleMdxFiles } from './localeMdxUtils'
+export { deleteLocaleMdxFiles, removeLocalizesFromLocaleFiles } from './localeMdxUtils'
 
 // Git sync
-export {
-  type SyncContext,
-  getTargetRepoRoot,
-  resolveTargetRepoPath,
-  validateGitSyncRepoOnStartup,
-  scheduleGitSync,
-  gitCommitAndPush
-} from './gitSync'
+export { type SyncContext, getTargetRepoRoot, resolveTargetRepoPath, validateGitSyncRepoOnStartup, scheduleGitSync, gitCommitAndPush } from './gitSync'
 
 // Lifecycle factories
-export {
-  shouldSkipMdxExport,
-  getAdminAuthor,
-  type PageLifecycleConfig,
-  type StrapiDocumentServiceUpdateWhere,
-  resolvePageFilepath,
-  generateMDX,
-  createPageLifecycle,
-  readLocaleFromUpdateEvent
-} from './pageLifecycle'
+export { type PageLifecycleConfig, type StrapiDocumentServiceUpdateWhere, shouldSkipMdxExport, getAdminAuthor, resolvePageFilepath, generateMDX, createPageLifecycle, readLocaleFromUpdateEvent } from './pageLifecycle'
 export { createBlogLifecycle } from './blogLifecycle'
-export {
-  type FlatContentLifecycleConfig,
-  type FlatLocaleMdxLifecycleConfig,
-  createFlatLocaleMdxLifecycle
-} from './flatContentLifecycle'
+export { type FlatContentLifecycleConfig, type FlatLocaleMdxLifecycleConfig, createFlatLocaleMdxLifecycle } from './flatContentLifecycle'
 export { type NavigationLifecycleConfig, createNavigationLifecycle } from './navigationLifecycle'
