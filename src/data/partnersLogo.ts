3512a7c4ac707c94c57c0aa5d90719852cf2dbef
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2026-07-08 10:44:08 +0300

    refactor(components): make LogoCarousel reusable

---
+++ src/data/partnersLogo.ts
@@ -0,0 +1,67 @@
+// All source logos are 240×80 px, rendered at 120×40 for 2x retina sharpness.
+export const partners = [
+  {
+    name: 'Alabama A&M University',
+    src: '/img/partner-logos/Alabama-AM.png'
+  },
+  {
+    name: 'The AUC Data Science Initiative',
+    src: '/img/partner-logos/AUCDSI.png'
+  },
+  {
+    name: 'BessPay',
+    src: '/img/partner-logos/BessPay.png'
+  },
+  {
+    name: 'Confab',
+    src: '/img/partner-logos/Confab.png'
+  },
+  {
+    name: 'Instituto Politécnico Nacional ESCOM',
+    src: '/img/partner-logos/Escudo-ESCOM.png'
+  },
+  {
+    name: 'Gatehub',
+    src: '/img/partner-logos/Gatehub.png'
+  },
+  {
+    name: 'ISSRA - Institute for Strategic Studies Research & Analysis',
+    src: '/img/partner-logos/ISSRA.png'
+  },
+  {
+    name: 'Mydata Rights',
+    src: '/img/partner-logos/Mydata-rights.png'
+  },
+  {
+    name: 'Nyu',
+    src: '/img/partner-logos/Nyu.png'
+  },
+  {
+    name: 'Paysys',
+    src: '/img/partner-logos/Paysys.png'
+  },
+  {
+    name: 'Plata',
+    src: '/img/partner-logos/plata.png'
+  },
+  {
+    name: 'Smashing Magazine',
+    src: '/img/partner-logos/Smashing-Magazine.png'
+  },
+  {
+    name: 'The Hague University of Applied Sciences',
+    src: '/img/partner-logos/The-Hague-university-of-applied-sciences.png'
+  },
+  {
+    name: 'University of Cape Town',
+    src: '/img/partner-logos/University-of-Cape-Town-UCT.png'
+  },
+  {
+    name: 'The University of Hong Kong',
+    src: '/img/partner-logos/University-of-Hong-Kong.png'
+  },
+  {
+    name: 'Western Sydney University',
+    src: '/img/partner-logos/Western-Sydney-university.png'
+  }
+]
