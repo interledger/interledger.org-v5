c18390dd64c4965beae279c14646fe0ceeb71344
Jonathan Matthey <mattheyj@gmail.com>
2026-07-11 17:30:57 +0200

    update homepage partner logos

--- src/data/partnersLogo.ts
+++ src/data/partnersLogo.ts
@@ -1,67 +1,123 @@
 // All source logos are 240×80 px, rendered at 120×40 for 2x retina sharpness.
 export const partners = [
   {
-    name: 'Alabama A&M University',
-    src: '/img/partner-logos/Alabama-AM.png'
-  },
-  {
-    name: 'The AUC Data Science Initiative',
-    src: '/img/partner-logos/AUCDSI.png'
-  },
-  {
-    name: 'BessPay',
-    src: '/img/partner-logos/BessPay.png'
-  },
-  {
-    name: 'Confab',
-    src: '/img/partner-logos/Confab.png'
-  },
-  {
-    name: 'Instituto Politécnico Nacional ESCOM',
-    src: '/img/partner-logos/Escudo-ESCOM.png'
-  },
-  {
-    name: 'Gatehub',
-    src: '/img/partner-logos/Gatehub.png'
-  },
-  {
-    name: 'ISSRA - Institute for Strategic Studies Research & Analysis',
-    src: '/img/partner-logos/ISSRA.png'
-  },
-  {
-    name: 'Mydata Rights',
-    src: '/img/partner-logos/Mydata-rights.png'
-  },
-  {
-    name: 'Nyu',
-    src: '/img/partner-logos/Nyu.png'
-  },
-  {
-    name: 'Paysys',
-    src: '/img/partner-logos/Paysys.png'
-  },
-  {
-    name: 'Plata',
-    src: '/img/partner-logos/plata.png'
-  },
-  {
-    name: 'Smashing Magazine',
-    src: '/img/partner-logos/Smashing-Magazine.png'
-  },
-  {
-    name: 'The Hague University of Applied Sciences',
-    src: '/img/partner-logos/The-Hague-university-of-applied-sciences.png'
-  },
-  {
-    name: 'University of Cape Town',
-    src: '/img/partner-logos/University-of-Cape-Town-UCT.png'
-  },
-  {
-    name: 'The University of Hong Kong',
-    src: '/img/partner-logos/University-of-Hong-Kong.png'
+    name: 'Covenant',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0000_Covenant_logo-328x350.png'
   },
   {
     name: 'Western Sydney University',
-    src: '/img/partner-logos/Western-Sydney-university.png'
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0001_WSU_Logo_LeftAligned_Centred_RGB.png'
+  },
+  {
+    name: 'USIU',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0002_USIU-Logo-with-Tagline.png'
+  },
+  {
+    name: 'University of Cape Town',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0003_UCT-Logo.png'
+  },
+  {
+    name: 'HHS',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0004_HHS_EN_grijs_HEX.png'
+  },
+  {
+    name: 'Boise State University',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0005_bsu-logo_yllw-flame-blk-text_highres-(1).png'
+  },
+  {
+    name: 'Atlanta University Center Consortium',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0006_Atlanta-UCConsortium.png'
+  },
+  {
+    name: 'Alabama A&M University',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0007_AAMU-logo.png'
+  },
+  {
+    name: 'KANZU',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0008_KANZU.png'
+  },
+  {
+    name: 'Wallet Guru',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0009_Wallet-Guru.png'
+  },
+  {
+    name: 'W3C',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0010_W3C-Symbol.png'
+  },
+  {
+    name: 'Social Web Foundation',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0011_swf-logo-black-on-white---Evan-Prodromou.png'
+  },
+  {
+    name: 'Super Bloom',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0012_Super-Bloom-.png'
+  },
+  {
+    name: 'Small Foundation',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0013_small-foundation-logo-vector.png'
+  },
+  {
+    name: 'Ripple',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0014_Ripple_(payment_protocol)-Logo.wine.png'
+  },
+  {
+    name: 'PCH',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0015_PCH-Banner-Azul_-sinFondo.png'
+  },
+  {
+    name: 'Open Wallet Foundation',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0016_OpenWallet_Foundation_Logo_Color.png'
+  },
+  {
+    name: 'Nepal Internet Foundation',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0017_NIF-Logo---Nepal-Internet-Foundation.png'
+  },
+  {
+    name: 'Mojaloop',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0018_Mojaloop.png'
+  },
+  {
+    name: 'MiPlata',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0019_MiPlata-Logo---Blue---Loreto-Zumalacarregui-Calderón.png'
+  },
+  {
+    name: 'CTS',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0020_Marca_CTS---Hannah-JD.png'
+  },
+  {
+    name: 'JoPACC',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0021_jopacc_new_logo_transparent_0.png'
+  },
+  {
+    name: 'Gatehub',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0022_Gatehub_Logo.png'
+  },
+  {
+    name: 'Fliqa',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0023_Fliqa_logo.png'
+  },
+  {
+    name: 'CG',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0024_CG-Banner-Azul_sinFondo.png'
+  },
+  {
+    name: 'Breakpoint',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0025_Breakpoint_logo_square.png'
+  },
+  {
+    name: 'Blaze Group',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0026_Blaze-group.png'
+  },
+  {
+    name: 'Article 19',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0027_Article_19_logo.svg.png'
+  },
+  {
+    name: 'Alliance',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0028_Alliance_MainLogo_New---Sarah-Corley.png'
+  },
+  {
+    name: 'Alabama A&M University',
+    src: '/img/partner-logos/LOGO-STRIP-HOME-PAGE-V2_0029_AAMU-logo.png'
   }
 ]
